8aefedf1944b9ca9de128760db105c740ce8ad85
Matianxing <450086055@qq.com>
2026-07-07 14:53:02 +0800

    fix: add pt-[100px] to HeroSection to clear fixed navbar height

--- docs/.vitepress/theme/components/home/HeroSection.vue
+++ docs/.vitepress/theme/components/home/HeroSection.vue
@@ -4,7 +4,7 @@ import KnowledgeCore from './KnowledgeCore.vue'
 </script>
 
 <template>
-  <section class="min-h-screen flex items-center max-w-[1200px] mx-auto px-[120px] max-lg:px-12 max-sm:px-6">
+  <section class="min-h-screen flex items-center max-w-[1200px] mx-auto px-[120px] pt-[100px] max-lg:px-12 max-sm:px-6">
     <div class="flex items-center gap-12 w-full">
       <div class="flex-1">
         <HeroContent />
